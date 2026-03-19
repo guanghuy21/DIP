b2b61ca118b268fdfc3fba7d7a7af1e8a53a7e02
Huy <quanghuycsp19@gmail.com>
2026-03-19 17:47:56 +0800

    VNA_WORKER updates

--- api/gateway.py
+++ api/gateway.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, make_response
 from api.routes import api_blueprint  # Import the routes
 from flask_cors import CORS
-
+import socket
 app = Flask(__name__)
 CORS(app)
 # 1. Register the routes from routes.py
@@ -17,6 +17,9 @@ def server_error(error):
     return make_response(jsonify({"error": "Internal Server Error"}), 500)
 
 def start_api_gateway(host="127.0.0.1", port=3000):
+    lan_ip = socket.gethostbyname(socket.gethostname())
+    if lan_ip: host = lan_ip
+    # print(f"API running at http://{lan_ip}:{port}")
     print(f"[*] API Gateway starting on http://{host}:{port}")
     # We disable the reloader because it can conflict with the Boss's threads
     app.run(host=host, port=port, debug=True, use_reloader=True)
